1690b1a6ce43c6dd45dea07a6c887283a26da539
nirvanarockk@hotmail.com <nirvanarockk@hotmail.com>
2019-03-25 08:59:47 -0600

    We added code un html and main.js to give users the option for log in

---
+++ main.js
@@ -0,0 +1,98 @@
+console.log('tudo bem');
+function registrar() {
+    console.log('diste click');
+    let email =document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then (function(){
+        verify ()
+    })
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode);
+        
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        
+        // ...
+      });
+}
+
+function ingreso (){
+    let newEmail =document.getElementById('new-email').value;
+    let newPassword = document.getElementById('new-password').value;
+
+    firebase.auth().signInWithEmailAndPassword(newEmail, newPassword).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode);
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        
+        // ...
+      });
+      
+      
+}
+function observador () {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            console.log('existe usuario');
+            show()
+            
+          // User is signed in.
+          var displayName = user.displayName;
+          var email = user.email;
+          var emailVerified = user.emailVerified;
+          var photoURL = user.photoURL;
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          var providerData = user.providerData;
+          // ...
+        } else {
+          // User is signed out.
+          // ...
+          console.log('no existe usuario');
+          
+        }
+      });
+      
+}
+
+observador();
+
+function show(){
+    let content = document.getElementById('conteiner')
+    content.innerHTML = `
+    <p>Solo usuarios registrados pueden verlo </p>
+    <button onclick="closeSession()">Cerrar seciòn</button>
+    `;
+}
+
+function closeSession(){
+    firebase.auth().signOut()
+    .then(function (){
+        console.log('saliendo...');
+        
+    })
+    .catch(function (error){
+        console.log('error no has salido');
+        
+    })
+}
+
+function verify () {
+    var user = firebase.auth().currentUser;
+
+user.sendEmailVerification().then(function() {
+  // Email sent.
+  console.log('enviando correo...');
+  
+}).catch(function(error) {
+    console.log('no se envio correo');
+    
+  // An error happened.
+});
+}
