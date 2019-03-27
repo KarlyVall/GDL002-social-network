3052a079808ae4e2e0f719e7c71e9d8ed1d812ad
lumenbarron <lucemendez@gmail.com>
2019-03-27 12:57:32 -0600

    Inicialitatio of cloud fiestone

--- main.js
+++ main.js
@@ -8,34 +8,25 @@
   messagingSenderId: "804727554993"
 };
 firebase.initializeApp(config);
-// const db = firebase.firestore();
-// db.settings({timestampsInSnapshots:true});
 
-// db.collection('usuarios').get().then((snapshot) => {
-//   snapshot.docs.forEach(doc => {
-//     console.log(doc.data());
-    
-//   });
-  
-// })
+// Initialize Cloud Firestore through Firebase
+const db = firebase.firestore();
+
+
 
 //Function to register new users
 const register = () => {
     let email =document.querySelector('#email').value;
-    
     let password = document.querySelector('#password').value;
-    
-    let name = document.querySelector('#user').value
-  
+    let name = document.querySelector('#user').value;
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then (function(){
         verify ()
-        createUserDatabase (email, name)
+        saveData()
     })
     .catch(function(error) {
         // Handle Errors here.
         let errorCode = error.code;
-        
         let errorMessage = error.message;
         
         // ...
@@ -58,26 +49,87 @@ const login =  () => {
       
 }
 // Get a reference to the database service
-let database = firebase.database();
+//let database = firebase.database();
 
 //Function to create datebase of users (registred)
-const createUserDatabase = (email, name) => {
-  console.log('estoy funcionando');
-  console.log(uid);
-    // Get a id of user 
-  // let userId = firebase.auth().currentUser.uid;
-  // console.log(userId);
-  
-  firebase.database().ref('usuarios/' + uid).set({
-    username: name,
-    email: email,
-    id: uid,
-    profile_picture : 'imageUrl'
+// const createUserDatabase = (email, name) => {
+//   console.log('estoy funcionando');
+//   console.log(email);
+//   console.log(name);
+//   console.log(uid);
+//     // Get a id of user 
+//   // let userId = firebase.auth().currentUser.uid;
+//   // console.log(userId);
+//   firebase.database().ref('usuarios/' + uid).set({
+//     username: name,
+//     email: email,
+//     id: uid,
+//     profile_picture : 'imageUrl'
 
+//   });
+// }
+
+
+//Add user´s data
+const saveData = () => {
+  console.log('estoy con save data');
+  let email = document.querySelector('#email').value;
+  let name = document.querySelector('#user').value;
+  db.collection("usuarios").add({
+    nameUser: name,
+    emailUser: email,
+
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+    document.querySelector('#email').value= " ";
+    document.querySelector('#user').value = " ";
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+}
+
+// Get a reference to the database service
+//let database = firebase.database();
+
+//Function to create datebase of users (registred)
+// const createUserDatabase = (email, name) => {
+//   console.log('estoy funcionando');
+//   console.log(email);
+//   console.log(name);
+//   console.log(uid);
+//     // Get a id of user 
+//   // let userId = firebase.auth().currentUser.uid;
+//   // console.log(userId);
+//   firebase.database().ref('usuarios/' + uid).set({
+//     username: name,
+//     email: email,
+//     id: uid,
+//     profile_picture : 'imageUrl'
+
+//   });
+// }
+
+
+//Add user´s data
+const saveComent = () => {
+  console.log('estoy funcionando');
+  let text = document.querySelector('#article').value;
+  db.collection("posts").add({
+    textuser : text
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
   });
 }
 
 let uid;
+
+
 //Function to observer validation
 const observer = () => {
     firebase.auth().onAuthStateChanged(function(user) {
@@ -86,9 +138,7 @@ const observer = () => {
             show()
             
           // User is signed in.
-          let displayName = user.displayName;
-          console.log(displayName);
-          
+          let displayName = user.displayName;          
           let email = user.email;
           let emailVerified = user.emailVerified;
           let photoURL = user.photoURL;
@@ -109,19 +159,26 @@ const observer = () => {
 
 observer();
 
+
 //Function to show content when user do login
 const show = () => {
-    let content = document.getElementById('conteiner')
-    content.innerHTML = `
-    <p>Solo usuarios registrados pueden verlo </p>
-    <button onclick="closeSession()">Cerrar sesión</button>
-    `;
+    let publicar = document.getElementById('conteiner')
+    publicar.style.display = "block";
+    // let content = document.getElementById('prueba')
+    // content.innerHTML = `
+    // <h2>Mi muro</h2>
+    // <p>Solo usuarios registrados pueden verlo </p>
+    // <button onclick="closeSession()">Cerrar sesión</button>
+    // `;
+    
+
 }
 //Function to logOut
 const closeSession = () => {
     firebase.auth().signOut()
     .then(function (){
         console.log('saliendo...');
+       
         
     })
     .catch(function (error){
