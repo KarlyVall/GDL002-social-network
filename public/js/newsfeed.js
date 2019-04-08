2f19662b5a923ba8785fbdf9b3552f0ff1b3f652
marisolcastrolopez <46985112+marisolcastrolopez@users.noreply.github.com>
2019-04-07 19:01:51 -0500

    Color testing UX

--- public/js/newsfeed.js
+++ public/js/newsfeed.js
@@ -10,16 +10,16 @@ const newsFeedJs = () => {
 
       newsfeedSection.innerHTML += `
       <div class="card">
-               <div class="card-section grid-x">
-                  <img class="cell small-2 profile-pic" src="img/img-profile-baby.png">
-                  <p class="cell small-9 user-name">${doc.data().email}<br>Categoría: ${doc.data().typeArticle}</p>
-                  </div>
-                  <div class="grid-x">
-                  <span class="cell small-12 post-text">${doc.data().textuser}</span>
-               </div>
-               <!--like botton-->
-      <div class="fb-like" data-href="https://compartiendo-sonrisas.firebaseio.com/" data-layout="button_count"
-         data-action="like" data-size="small" data-show-faces="true" data-share="false"></div>
+         <div class="card-section grid-x">
+            <img class="cell small-2 profile-pic" src="img/img-profile-baby-warm.png">
+            <p class="cell small-9 user-name"><span class="bold-font" style="color: #f4897a;">${doc.data().email}</span><br><span class="bold-font">Categoría:</span> <span class="normal-font">${doc.data().typeArticle}</span></p>
+         </div>
+         <div class="grid-x">
+            <span class="cell small-12 post-text">${doc.data().textuser}</span>
+         </div>
+         <!--like button-->
+         <div class="fb-like" data-href="https://compartiendo-sonrisas.firebaseio.com/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="true" data-share="false">
+         </div>
       </div>
       `
   });
